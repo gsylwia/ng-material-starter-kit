88aed852aaf89f34ce92156bda109b0aed6ed57f
Sylwia <g.sylwia@vp.pl>
2023-01-03 23:47:37 +0100

    feat: categories checkbox list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CryptoListComponent } from './components/crypto-list/crypto-list.component';
+import { CategoriesCheckboxComponent } from './components/categories-checkbox/categories-checkbox.component';
 import { CryptoListComponentModule } from './components/crypto-list/crypto-list.component-module';
+import { CategoriesCheckboxComponentModule } from './components/categories-checkbox/categories-checkbox.component-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([{ path: 'crypto', component: CryptoListComponent }]), CryptoListComponentModule],
+  imports: [RouterModule.forRoot([{ path: 'crypto', component: CryptoListComponent }, { path: 'checkbox-categories', component: CategoriesCheckboxComponent }]), CryptoListComponentModule, CategoriesCheckboxComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
