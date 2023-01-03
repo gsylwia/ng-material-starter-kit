2e3af871a36e7da6c5295ffb0853eb3b23088316
Sylwia <g.sylwia@vp.pl>
2023-01-04 00:02:36 +0100

    feat: categories menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CryptoListComponent } from './components/crypto-list/crypto-list.component';
 import { CategoriesCheckboxComponent } from './components/categories-checkbox/categories-checkbox.component';
+import { CategoriesMenuComponent } from './components/categories-menu/categories-menu.component';
 import { CryptoListComponentModule } from './components/crypto-list/crypto-list.component-module';
 import { CategoriesCheckboxComponentModule } from './components/categories-checkbox/categories-checkbox.component-module';
+import { CategoriesMenuComponentModule } from './components/categories-menu/categories-menu.component-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([{ path: 'crypto', component: CryptoListComponent }, { path: 'checkbox-categories', component: CategoriesCheckboxComponent }]), CryptoListComponentModule, CategoriesCheckboxComponentModule],
+
+  imports: [RouterModule.forRoot([{ path: 'crypto', component: CryptoListComponent }, { path: 'checkbox-categories', component: CategoriesCheckboxComponent }, { path: 'categories-menu', component: CategoriesMenuComponent }]), CryptoListComponentModule, CategoriesCheckboxComponentModule, CategoriesMenuComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
