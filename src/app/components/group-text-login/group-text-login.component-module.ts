4abed1cc6a11ce6c12e391ed857d519817bdac2d
Sylwia <g.sylwia@vp.pl>
2023-01-13 00:28:24 +0100

    fix: form group text login layout

--- src/app/components/group-text-login/group-text-login.component-module.ts
+++ src/app/components/group-text-login/group-text-login.component-module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { GroupTextLoginComponent } from './group-text-login.component';
 
 @NgModule({
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, CommonModule, MatButtonModule],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, CommonModule, MatButtonModule, MatCardModule],
   declarations: [GroupTextLoginComponent],
   providers: [],
   exports: [GroupTextLoginComponent]
